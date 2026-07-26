d8c79198b995502c3ace021f164eea3a085c2ea2
Pankaj <shubhu2004sharma@gmail.com>
2026-07-26 21:09:39 +0530

    feat: add basic error handling for create route
    Wrapped create listing route in try-catch and added a basic error-handling middleware

--- app.js
+++ app.js
@@ -49,10 +49,15 @@ app.get("/listings/:id", async (req, res) => {
 });
 
 //CREATE ROUTE
-app.post("/listings", async (req, res) => {
-  const newListing = new Listing(req.body.listing);
-  await newListing.save();
-  res.redirect("/listings");
+app.post("/listings",async(req,res,next)=>{
+  try{
+      console.log(req.body);
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+  } catch (err){
+    next(err);
+  }
 });
 
 //EDIT ROUTE
@@ -94,6 +99,10 @@ app.delete("/listings/:id", async (req, res) => {
 //     console.log("sample was saved");
 //     res.send("successful testing");
 // });
+
+app.use((err,req,res,next) => {
+  res.send("something went wrong!");
+});
 
 app.listen(8080, () => {
   console.log("server is listening to port 8080");
