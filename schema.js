bdf48efbb3350bfc6e91578bd4149f01eaa84b8f
Pankaj <shubhu2004sharma@gmail.com>
2026-07-28 00:16:35 +0530

    fix: correct image field validation in listing schema
    Updated Joi schema to validate image as an object with a url property, matching the actual listing model structure

--- schema.js
+++ schema.js
@@ -1,14 +1,27 @@
 const Joi = require('joi');
 
 module.exports.listingSchema = Joi.object({
-    listing : Joi.object({
-        title: Joi.string().required(),
-        description: Joi.string().required(),
-        location: Joi.string().required(),
-        country: Joi.string().required(),
-        price: Joi.number().required().min(0),
-        image: Joi.string().allow("",null),
-    }).required()
+
+listing:Joi.object({
+
+title:Joi.string().required(),
+
+description:Joi.string().required(),
+
+location:Joi.string().required(),
+
+country:Joi.string().required(),
+
+price:Joi.number().required().min(0),
+
+image:Joi.object({
+
+url:Joi.string().allow("",null)
+
+}).required()
+
+}).required()
+
 });
 
 module.exports.reviewSchema = Joi.object({
