eb6258a15e2cf55ada1ddc74eb0cc3b5e5bb20d0
Pankaj <shubhu2004sharma@gmail.com>
2026-07-27 17:41:28 +0530

    feat: add review deletion and improve reviews styling
    Added delete route for individual reviews with delete button on show page, and updated card styling for listing and review sections

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema , reviewSchema } = require("./schema.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
 const Review = require("./models/review.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -37,22 +37,22 @@ app.get("/", (req, res) => {
 });
 
 const validateListing = (req, res, next) => {
-  let {error} = listingSchema.validate(req.body);
+  let { error } = listingSchema.validate(req.body);
   if (error) {
     let errMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400,errMsg);
-  }else {
-    next(); 
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
   }
 };
- 
+
 const validateReview = (req, res, next) => {
-  let {error} = reviewSchema.validate(req.body);
+  let { error } = reviewSchema.validate(req.body);
   if (error) {
     let errMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400,errMsg);
-  }else {
-    next(); 
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
   }
 };
 
@@ -82,7 +82,8 @@ app.get(
 
 //CREATE ROUTE
 app.post(
-  "/listings",validateListing,
+  "/listings",
+  validateListing,
   wrapAsync(async (req, res, next) => {
     const newListing = new Listing(req.body.listing);
     await newListing.save();
@@ -102,7 +103,8 @@ app.get(
 
 //UPDATE ROUTE
 app.put(
-  "/listings/:id",validateListing,
+  "/listings/:id",
+  validateListing,
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, req.body.listing);
@@ -122,17 +124,40 @@ app.delete(
 );
 
 // REVIEWS - POST ROUTE
-app.post("/listings/:id/reviews", validateReview, wrapAsync(async(req, res) => {
-  let listing = await Listing.findById(req.params.id);
-  let newReview = new Review(req.body.review);
+app.post(
+  "/listings/:id/reviews",
+  validateReview,
+  wrapAsync(async (req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
 
-  listing.reviews.push(newReview);
+    listing.reviews.push(newReview);
 
-  await newReview.save();
-  await listing.save();
+    await newReview.save();
+    await listing.save();
 
-  res.redirect(`/listings/${listing._id}`);
-}));
+    res.redirect(`/listings/${listing._id}`);
+  }),
+);
+
+// REVIEWS - DELETE ROUTE
+app.delete(
+  "/listings/:id/reviews/:reviewId",
+  wrapAsync(async (req, res) => {
+    let { id, reviewId } = req.params;
+
+    await Listing.findByIdAndUpdate(
+      id,
+       {
+         $pull:{
+           reviews: reviewId 
+          }
+        });
+    await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
+  }),
+);
 
 // app.get("/testListing", async (req,res) => {
 //     let sampleListing = new Listing ({
