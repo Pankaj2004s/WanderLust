9dad1c924446274aad343cb7f86c475d683ee4e8
Pankaj <shubhu2004sharma@gmail.com>
2026-07-28 15:26:16 +0530

    feat: implement flash messages for success and failure notifications
    Added connect-flash middleware, created flash message partial for success/error alerts, included it in boilerplate layout, and added flash triggers on create, update, delete, and not-found scenarios for listings and reviews

--- routes/listing.js
+++ routes/listing.js
@@ -35,6 +35,10 @@ router.get(
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id).populate("reviews");
+    if(!listing) {
+      req.flash("error", "Listing you requested for does not exists!");
+      return res.redirect("/listings");
+    }
     res.render("listings/show", { listing });
   }),
 );
@@ -46,6 +50,7 @@ router.post(
   wrapAsync(async (req, res, next) => {
     const newListing = new Listing(req.body.listing);
     await newListing.save();
+    req.flash("success", "New Listing Created!");
     res.redirect("/listings");
   }),
 );
@@ -67,6 +72,7 @@ router.put(
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, req.body.listing);
+    req.flash("success", "Listing Updated!");
     res.redirect(`/listings/${id}`);
   }),
 );
@@ -78,6 +84,7 @@ router.delete(
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
+    req.flash("success", "Listing Deleted!");
     res.redirect("/listings");
   }),
 );
--- routes/review.js
+++ routes/review.js
@@ -22,12 +22,16 @@ router.post(
   validateReview,
   wrapAsync(async (req, res) => {
     let listing = await Listing.findById(req.params.id);
+    if (!listing) {
+    req.flash("error", "Listing doesn't exist!");
+    return res.redirect("/listings");
+}
     let newReview = new Review(req.body.review);
 
     listing.reviews.push(newReview);
     await newReview.save();
     await listing.save();
-
+    req.flash("success", "New Review Created!");
     res.redirect(`/listings/${listing._id}`);
   }),
 );
@@ -44,7 +48,7 @@ router.delete(
       },
     });
     await Review.findByIdAndDelete(reviewId);
-
+    req.flash("success", "Review Deleted!");
     res.redirect(`/listings/${id}`);
   }),
 );
