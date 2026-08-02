6044137711f155cc550dbcd4103b4f8c7156c0fa
Pankaj <shubhu2004sharma@gmail.com>
2026-08-02 21:09:47 +0530

    feat: make tax switch functional to toggle GST display

--- public/js/script.js
+++ public/js/script.js
@@ -17,6 +17,13 @@
 let taxSwitch = document.getElementById("switchCheckDefault");
 if (taxSwitch) {
   taxSwitch.addEventListener("click", () => {
-    console.log("clicked");
+    let taxInfo = document.getElementsByClassName("tax-info");
+    for(let info of taxInfo) {
+      if(info.style.display != "inline") {
+        info.style.display = "inline";
+      } else {
+        info.style.display = "none";
+      }
+    }
   });
 }
